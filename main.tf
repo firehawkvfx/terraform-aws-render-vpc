b37786ea4dc952963ccf8b53abf732cbbfea43b4
Andrew Graham <queglay@gmail.com>
2022-05-30 21:03:41 +0930

    update version for firehawk main

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ locals {
 }
 module "vpc" {
   # source                       = "./modules/terraform-aws-vpc"
-  source                       = "github.com/firehawkvfx/firehawk-main.git//modules/vpc/modules/terraform-aws-vpc?ref=v0.0.33"
+  source                       = "github.com/firehawkvfx/firehawk-main.git//modules/vpc/modules/terraform-aws-vpc?ref=v0.0.47"
   enable_nat_gateway           = true
   vpc_name                     = local.common_tags["vpcname"]
   vpc_cidr                     = module.rendervpc_all_subnet_cidrs.base_cidr_block
